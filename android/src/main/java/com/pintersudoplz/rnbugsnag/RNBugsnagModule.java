6f079e5444be4ff43c5bde780d06b3162f394041
John Kokkinidis <sudoplz@gmail.com>
2016-07-03 02:48:09 +0300

    - Added some initial code to both iOS and Android.

--- android/src/main/java/com/pintersudoplz/rnbugsnag/RNBugsnagModule.java
+++ android/src/main/java/com/pintersudoplz/rnbugsnag/RNBugsnagModule.java
@@ -7,6 +7,13 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+//BUGSNAG ANDROID API HERE : http://docs.bugsnag.com/platforms/android/#installation
+
+// import com.bugsnag.android.Bugsnag;
+// import com.bugsnag.android.MetaData;
+// import com.bugsnag.android.Severity;
+
+
 class RNBugsnagModule extends ReactContextBaseJavaModule {
     private Context context;
 
@@ -34,13 +41,76 @@ class RNBugsnagModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void setIdentifier(String userId, String email, String fullName, Callback onSuccess, Callback onFailure) {
         //This gets called whenever setIdentifier is invoked from javascript
+        
+        // Bugsnag.setUser(userId, email, fullName);
+
         onSuccess.invoke("User identified!");
     }
 
     @ReactMethod
     public void reportException(String errorMessage, ReadableArray stacktrace, Integer exceptionId, Boolean isFatal, Callback onSuccess, Callback onFailure) {
         //This gets called whenever a js error gets thrown
+
+
+        Error error = new Error(title);
+        error.setStackTrace(stackTraceToStackTraceElement(stacktrace));
+
+        MetaData metaData = new MetaData();
+        metaData.addToTab("Custom", "Stacktrace", stackTraceToString(stacktrace));
+
+
+        if(isFatal){
+            Bugsnag.notify(error, Severity.ERROR, metaData);
+        }else{
+            Bugsnag.notify(error, Severity.WARNING, metaData);
+        }
+        
+
+
+
         onSuccess.invoke("Error!");
+    }
+
+
+
+
+
+
+
+
+
+
+    private StackTraceElement[] stackTraceToStackTraceElement(ReadableArray stack) {
+        StackTraceElement[] stackTraceElements = new StackTraceElement[stack.size()];
+        for (int i = 0; i < stack.size(); i++) {
+            ReadableMap frame = stack.getMap(i);
+            stackTraceElements[i] = new StackTraceElement(
+                    "ReactJS",
+                    frame.getString("methodName"),
+                    new File(frame.getString("file")).getName(),
+                    frame.getInt("lineNumber")
+            );
+        }
+        return stackTraceElements;
+    }
+
+     private String stackTraceToString(ReadableArray stack) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++) {
+            ReadableMap frame = stack.getMap(i);
+            stringBuilder.append(frame.getString("methodName"));
+            stringBuilder.append("\n    ");
+            stringBuilder.append(new File(frame.getString("file")).getName());
+            stringBuilder.append(":");
+            stringBuilder.append(frame.getInt("lineNumber"));
+            if (frame.hasKey("column") && !frame.isNull("column")) {
+                stringBuilder
+                        .append(":")
+                        .append(frame.getInt("column"));
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 
 }
--- ios/RCTRNBugsnag/RCTRNBugsnag.m
+++ ios/RCTRNBugsnag/RCTRNBugsnag.m
@@ -1,17 +1,33 @@
 #import "RCTRNBugsnag.h"
+//#import "Bugsnag.h"
+
 
 @implementation RNBugsnag
 
+//BUGSNAG IOS API HERE : http://docs.bugsnag.com/platforms/ios/#installation
+
+
 RCT_EXPORT_MODULE();
 
+
+
+
+
 RCT_EXPORT_METHOD(init,
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
     
     //Run code that initializes BugSnag here
+    
+//    [Bugsnag startBugsnagWithApiKey:@"FETCH_THE_API_KEY_FROM_INFO_PLIST_AND_PASS_HERE"];
+
     resolve(@"Hello World!");
 }
+
+
+
+
 
 
 RCT_EXPORT_METHOD(setIdentifier:(NSString *)userId
@@ -20,24 +36,71 @@ RCT_EXPORT_METHOD(setIdentifier:(NSString *)userId
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
+        //This gets called whenever setIdentifier is invoked from javascript
     
-    //This gets called whenever setIdentifier is invoked from javascript
+    #ifdef DEBUG
+        reject(@"RNBugsnag won't report errors on dev mode");
+        return;
+    #endif
+    [[Bugsnag configuration] setUser:userId withName:fullName andEmail:email];
+    
     resolve(@"User identified");
 }
 
 
 
+
+
+
 RCT_EXPORT_METHOD(reportException:(NSString *)errorMessage
-                 withStack:(NSArray *)stacktrace
-                 andExceptionId:(NSInteger *)exceptionId
-                 isFatal:(BOOL) fatal
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
+                  withStack:(NSArray<NSDictionary *> *)stacktrace
+                  andExceptionId:(NSInteger *)exceptionId
+                  isFatal:(BOOL) fatal
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
     
     //This gets called whenever a js error gets thrown
+    
+    #ifdef DEBUG
+        reject(@"RNBugsnag won't report errors on dev mode");
+        return;
+    #endif
+    
+    
+    
+    NSMutableArray *stringFrameArray = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *stackFrame in stack) {
+        NSString *fileName = [NSString stringWithFormat:@"%@ @ %zd:%zd",
+                              [stackFrame[@"file"] lastPathComponent],
+                              [stackFrame[@"lineNumber"] integerValue],
+                              [stackFrame[@"column"] integerValue]];
+        
+        [stringFrameArray addObject:[NSString stringWithFormat:@"%@ %@", fileName, stackFrame[@"methodName"]]];
+    }
+    
+    NSDictionary *userInfo = @{
+                               NSLocalizedDescriptionKey: NSLocalizedString([stringFrameArray componentsJoinedByString:@"\n"], nil),
+                               };
+    
+    NSMutableDictionary *allErrorData = [errorData mutableCopy];
+    [allErrorData addEntriesFromDictionary:@{@"Stacktrace": [stringFrameArray componentsJoinedByString:@"\n"]}];
+    
+    NSString* severity;
+    if(isFatal){
+        severity = @"error"
+    }else{
+        severity = @"warning"
+    }
+//    [Bugsnag notify:[NSException exceptionWithName:errorMessage reason:[stringFrameArray componentsJoinedByString:@"\n"] userInfo:userInfo]
+//           withData:allErrorData atSeverity:severity];
+    
     resolve(@"Error");
 }
+
+
+
 
 
 
