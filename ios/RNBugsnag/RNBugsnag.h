523a5b59bcc5395f4cc7cc4141b77bcfa715be31
John Kokkinidis <sudoplz@gmail.com>
2016-07-21 18:05:03 +0300

    - Exposed the notify method to the public part of our module.

--- ios/RNBugsnag/RNBugsnag.h
+++ ios/RNBugsnag/RNBugsnag.h
@@ -3,4 +3,8 @@
 @interface RNBugsnag : NSObject <RCTBridgeModule>
 @property BOOL suppressDev;
 + (RNBugsnag*)init;
+- (void)notifyWithTitle: (NSString *)exceptionTitle
+              andReason:(NSString *)exceptionReason
+           withSeverity:(NSString *)severity
+           andOtherData:(NSDictionary *) otherData;
 @end
